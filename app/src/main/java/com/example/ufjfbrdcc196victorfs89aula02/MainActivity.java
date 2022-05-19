2c1b65249dc55a272a082cd392677a431087d4dd
Victor Santos <victorfons2@gmail.com>
2022-05-18 22:14:45 -0300

    Alterando o texto de um textView em tempo de execução

--- app/src/main/java/com/example/ufjfbrdcc196victorfs89aula02/MainActivity.java
+++ app/src/main/java/com/example/ufjfbrdcc196victorfs89aula02/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ufjfbrdcc196victorfs89aula02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textViewMensagem = findViewById(R.id.textViewMensagem);
+
+        textViewMensagem.setText("Olá Mundo!!!");
     }
 }
